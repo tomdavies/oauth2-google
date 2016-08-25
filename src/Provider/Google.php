bb587d01d2e4e03ee483e95650ab36bb12992410
Benjamin David <ben@dukt.net>
2016-08-25 22:46:16 +0200

    Fixed a bug where the resource owner details URL was wrong

--- src/Provider/Google.php
+++ src/Provider/Google.php
@@ -2,20 +2,17 @@
 
 namespace Dukt\OAuth2\Client\Provider;
 
-use League\OAuth2\Client\Provider\AbstractProvider;
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
-use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
-use Psr\Http\Message\ResponseInterface;
 
-class Google extends \League\OAuth2\Client\Provider\AbstractProvider\Google
+class Google extends \League\OAuth2\Client\Provider\Google
 {
     public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
-        $fields = array_merge($this->defaultUserFields, $this->userFields);
-        return 'https://www.googleapis.com/oauth2/v1/userinfo?' . http_build_query([
-            'fields' => implode(',', $fields),
-            'alt'    => 'json',
-        ]);
+        return 'https://www.googleapis.com/oauth2/v1/userinfo';
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new GoogleUser($response);
     }
 }
